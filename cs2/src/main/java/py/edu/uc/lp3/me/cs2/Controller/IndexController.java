8fa7aae5425d385ec034cbcd009691acb71a12ea
MatiasEnc <matiasencisoc@gmail.com>
2026-09-24 14:48:34 -0300

    Altualizar IndexController con respuesta de estado de API

--- cs2/src/main/java/py/edu/uc/lp3/me/cs2/Controller/IndexController.java
+++ cs2/src/main/java/py/edu/uc/lp3/me/cs2/Controller/IndexController.java
@@ -3,11 +3,17 @@ package py.edu.uc.lp3.me.cs2.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 public class IndexController {
 
 	@GetMapping("/")
-	public String index() {
-		return "Hola amigo";
-	}
+	public Map<String, String> index() {
+	   return Map.of(
+		 "autor", "Matias Enciso",
+		"dominio", "CS2",
+		"estado", "API funcionando"
+	   );
+      }
 }
